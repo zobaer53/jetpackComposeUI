acbe5074aab1bfadd5080e8fc9fb452f8659d445
zobaerTikweb <zobaer@tikweb.com>
2023-05-23 13:58:16 +0600

    final- practice compose

---
+++ android-compose-codelabs-main/BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/ui/theme/Color.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.codelab.basiclayouts.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val taupe100 = Color(0xFFF0EAE2)
+val taupe800 = Color(0xFF655454)
+val rust300 = Color(0xFFE1AFAF)
+val rust600 = Color(0xFF886363)
+val gray900 = Color(0xFF333333)
+val white150 = Color.White.copy(alpha = 0.15f)
+val white800 = Color.White.copy(alpha = 0.8f)
+val white850 = Color.White.copy(alpha = 0.85f)
+val gray800 = gray900.copy(alpha = 0.8f)
+val green200 = Color(0xffa5d6a7)
+val green500 = Color(0xff4caf50)
+val green700 = Color(0xff388e3c)
+
+val blue500 = Color(0xFF3F51B5)
+val blue200 = Color(0xFF9FA8DA)
+val blue700 = Color(0xFF303F9F)
+
+val purple200 = Color(0xFFB39DDB)
+val purple = Color(0xFF833AB4)
+val purple700 = Color(0xFF512DA8)
+
+val orange200 = Color(0xFFff7961)
+val orange500 = Color(0xFFf44336)
+val orange700 = Color(0xFFba000d)
+
+
+val teal200 = Color(0xff80deea)
+val twitterColor = Color(0xFF1DA1F2)
+val tiktokBlue = Color(0xFF69C9D0)
+val tiktokRed = Color(0xFFEE1D52)
+val tiktokBlack = Color(0xFF010101)
+val blue = Color(0xFF5851DB)
+
+val orange = Color(0xFFF56040)
+val yellow = Color(0xFFFCAF45)
+val graySurface = Color(0xFF2A2A2A)
