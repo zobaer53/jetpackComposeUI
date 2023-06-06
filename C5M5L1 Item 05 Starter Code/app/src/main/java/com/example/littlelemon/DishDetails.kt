c8618a555b7432f2d05dc37790a3a5cb42a4a85e
zobaer53 <hossain.zobaer11858@gmail.com>
2023-06-06 19:55:57 +0600

    Created the DishDetails Composable

--- C5M5L1 Item 05 Starter Code/app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ C5M5L1 Item 05 Starter Code/app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,20 +1,46 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
-    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        //TODO: Insert code here
+    Column(verticalArrangement = Arrangement.spacedBy(10.dp),
+    modifier = Modifier.padding(start = 10.dp, end = 10.dp)) {
+        TopAppBar()
+        Image(painter = painterResource(id = dish.imageResource),
+            contentDescription = "Dish image",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+            )
+        Column {
+            Text(text = dish.name,
+            style = MaterialTheme.typography.h1)
+            Text(text = dish.description,
+                style = MaterialTheme.typography.body1)
+            Counter()
+            Button(onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()
+                .align(CenterHorizontally)) {
+                Text(text = stringResource(id = R.string.add_for) + " $${dish.price}",
+                textAlign = TextAlign.Center)
+            }
+        }
     }
 }
 
